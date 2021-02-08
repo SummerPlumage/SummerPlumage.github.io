71f67e3b9be59eb4947d5ede29722eca542bd2f3
HKMC\H2101081 <A933097@ict-companion.com>
2021-02-08 09:30:04 +0900

    11

--- tempPost/index.js
+++ tempPost/index.js
@@ -1,18 +1,18 @@
 // let isDupl = false;
 // let arr = createNumber();
 // isDupl = isDuplicate(arr,isDupl);
-// console.log(arr, "중복여부: "+isDupl);     
+// console.log(arr, "중복여부: "+isDupl);
 // while(isDupl === true){
 //     if(isDupl === false){break;}
 //     arr = createNumber();
 //     isDupl = isDuplicate(arr,isDupl);
 // }
-// console.log(arr, "중복여부: "+isDupl);      
+// console.log(arr, "중복여부: "+isDupl);
 // function createNumber(){
 //     let arr = new Array(4);
 //     let index = 0;
-    
-//     for(let num of arr){                
+
+//     for(let num of arr){
 //         num = Math.floor(Math.random()*10);
 //         arr[index] = num;
 //         index++;
@@ -22,12 +22,12 @@
 // function isDuplicate(arr, isDupl)  {
 //     isDupl = arr.some(function(element) {
 //         return arr.indexOf(element) !== arr.lastIndexOf(element);
-//     });                           
+//     });
 //     return isDupl;
 // }
 
- 
-    
+
+
 //맨앞자리 0불가 로직추가
 
 // //배열
@@ -53,11 +53,11 @@
 //     arr = createNumber();
 //     isDupl = isDuplicate(arr,isDupl);
 // }
-// console.log(arr,"중복여부: "+isDupl);    
+// console.log(arr,"중복여부: "+isDupl);
 // function createNumber(){
 //     let arr = new Array(4);
 //     let index = 0;
-//     for(let num of arr){                
+//     for(let num of arr){
 //         num = Math.floor(Math.random()*10);
 //         arr[index] = num;
 //         index++;
@@ -67,17 +67,17 @@
 // function isDuplicate(arr, isDupl)  {
 //     isDupl = arr.some(function(element) {
 //         return arr.indexOf(element) !== arr.lastIndexOf(element);
-//     });                           
+//     });
 //     return isDupl;
 // }
 let isDupl = false;
 let arr = createNumber();
-// console.log(arr, "중복여부: "+isDupl); 
+// console.log(arr, "중복여부: "+isDupl);
 function createNumber(){
     let arr = new Array(4);
     let index = 0;
-    
-    for(let num of arr){                
+
+    for(let num of arr){
         num = Math.floor(Math.random()*10);
         arr[index] = num;
         index++;
@@ -85,39 +85,37 @@ function createNumber(){
     isDupl = isDuplicate(arr, isDupl);
     if(isDupl === true || arr[0] === 0) return createNumber();
     if(isDupl === false && arr[0] !== 0) return arr;
-    
+
 }
 function isDuplicate(arr, isDupl)  {
     isDupl = arr.some(function(element) {
         return arr.indexOf(element) !== arr.lastIndexOf(element);
-    });                           
+    });
     return isDupl;
 }
 
 //4자리 숫자 화면에띄우기
 
 let inputVal = "";
-window.onload = function() {    
+window.onload = function() {     //node에서 실행하면 window is not defined 에러로 실행불가함
     const btn = document.querySelector('.btn');
     const inputVal = document.querySelector('.input').value;
     const con1 = document.querySelector('.con1').innerHTML;
 
-} 
+}
 function clickOk(){
     inputVal = document.querySelector('.input').value;
-    
+
     // document.querySelector('.con1').innerHTML = inputVal;
     newContainer = document.createElement("div");
     newContainer.setAttribute("class","container");
     newContent = document.createElement("div");
     newContent.setAttribute("class","con1");
-    main = document.querySelector('#main');    
-    main.appendChild(newContainer);    
+    main = document.querySelector('#main');
+    main.appendChild(newContainer);
 
 
     container = document.querySelector('#container');
     container.appendChild(newContent);
     console.log(container);
 }
-
-
